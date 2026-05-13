350b02e299a2dfe055305ba77268c188abd3cb10
ash201 <25155040+ash201@users.noreply.github.com>
2026-05-13 09:46:42 +0530

    Added completed ML topics and projects

---
+++ Python/sample.py
@@ -0,0 +1,19 @@
+
+
+numbers = [10, 15, 20, 25, 30]
+count = 0
+for i in numbers:
+   # count = i[i > 20]
+   if i > 20:
+    count += 1
+
+    print(i)
+print(count)
+
+
+
+
+
+#Count how many numbers are greater than 20
+ 
+#20, 25 30
